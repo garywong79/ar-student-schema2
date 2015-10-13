6741c3c57655b38cded9517da92bcad4e7cc02ec
Gary Wong <garywong79@me.com>
2015-10-13 11:11:56 +0800

    validation magic

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,7 +1,13 @@
 require_relative '../../db/config'
 
+
 class Student < ActiveRecord::Base
 # implement your Student model here
+validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+validates :email, uniqueness: true
+validates :age, numericality: {greater_than: 5}
+validate :phone_must_have_10_digits_or_more
+
 	def name
 		"#{first_name} #{last_name}"
 	end
@@ -9,6 +15,13 @@ class Student < ActiveRecord::Base
 	def age
 		now = Date.today
     now.year - birthday.year - ((now.month > birthday.month || (now.month == birthday.month && now.day >= birthday.day)) ? 0 : 1)
+	end
+
+	private
+	def phone_must_have_10_digits_or_more
+		unless phone.gsub(/[^\d]/, '').length >=10
+			errors.add(:phone, "invalid phone number")
+		end
 	end
 
 end
