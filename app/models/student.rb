b8da07b87097e138084ab4a8da37bc333424b3a6
Gary Wong <garywong79@me.com>
2015-10-13 10:51:11 +0800

    your first object-relational model

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,13 @@ require_relative '../../db/config'
 
 class Student < ActiveRecord::Base
 # implement your Student model here
+	def name
+		"#{first_name} #{last_name}"
+	end
+
+	def age
+		now = Date.today
+    now.year - birthday.year - ((now.month > birthday.month || (now.month == birthday.month && now.day >= birthday.day)) ? 0 : 1)
+	end
+
 end
